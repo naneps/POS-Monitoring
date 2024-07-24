1e967ec335307391a02c19063e7b86e6a7910e78
naneps <ekaprasetya2244@gmail.com>
2024-07-24 22:07:55 +0700

    chore: Remove unused code and bindings for chat and todo modules

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,13 +1,7 @@
 import 'package:get/get.dart';
 
-import '../modules/chat/bindings/chat_binding.dart';
-import '../modules/chat/views/chat_view.dart';
-import '../modules/documentation/bindings/documentation_binding.dart';
-import '../modules/documentation/views/documentation_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
-import '../modules/todo/bindings/todo_binding.dart';
-import '../modules/todo/views/todo_view.dart';
 
 part 'app_routes.dart';
 
@@ -16,24 +10,9 @@ class AppPages {
 
   static final routes = [
     GetPage(
-      name: _Paths.TODO,
-      page: () => const TodoView(),
-      binding: TodoBinding(),
-    ),
-    GetPage(
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
-    ),
-    GetPage(
-      name: _Paths.CHAT,
-      page: () => const ChatView(),
-      binding: ChatBinding(),
-    ),
-    GetPage(
-      name: _Paths.DOCUMENTATION,
-      page: () => const DocumentationView(),
-      binding: DocumentationBinding(),
     ),
   ];
 
